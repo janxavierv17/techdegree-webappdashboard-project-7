3a2165ea95ca0645b0bd772e9f04ef46c8338a5c
janxavierv <37735268+janxavierv@users.noreply.github.com>
2020-12-08 19:59:28 +1100

    another small progress

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-var ctx = document.getElementById('traffic-chart').getContext('2d');
-var myChart = new Chart(ctx, {
+var line = document.getElementById('traffic-chart').getContext('2d');
+var lineChart = new Chart(line, {
     type: 'line',
     data: {
         labels: ['16-22', '23-29', '30-5', '6-12', '13-19', '29-26', '27-3', '4-10', '11-7', '18-24', '25-31'],
@@ -22,6 +22,47 @@ var myChart = new Chart(ctx, {
                     beginAtZero: true
                 }
             }]
+        }
+    }
+});
+var bar = document.getElementById("daily-chart").getContext("2d")
+var barChart = new Chart(bar, {
+    type: 'bar',
+    data: {
+        labels: ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+        datasets: [{
+            data: [75, 100, 175, 125, 225, 200, 100],
+            backgroundColor: [
+                'rgba(255, 255, 255, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255, 255, 255,255)',
+            ],
+            borderWidth: 1
+        }]
+    },
+
+});
+
+var doughnut = document.getElementById("mobileUsers-chart").getContext("2d")
+var barChart = new Chart(doughnut, {
+    type: 'doughnut',
+    data: {
+        labels: ["Phones", "Tablets", "Desktops"],
+        datasets: [{
+            data: [10,15,75],
+            backgroundColor: [
+                'rgba(255, 255, 255, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255, 255, 255,255)',
+            ],
+            borderWidth: 1, 
+            borderAlign: 'inner'
+        }]
+    },
+    options: {
+        scales: {
         }
     }
 });
